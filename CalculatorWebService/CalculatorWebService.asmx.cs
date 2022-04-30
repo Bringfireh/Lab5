932436bc7d1a9c3c1031ebad3c0e6bd004dababf
Bringfireh <ngutortsenongo@gmail.com>
2022-04-30 17:23:04 +0100

    added a Calculations helper and modified the description of webmethods

--- CalculatorWebService/CalculatorWebService.asmx.cs
+++ CalculatorWebService/CalculatorWebService.asmx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CalculatorWebService.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,16 +12,45 @@ namespace CalculatorWebService
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    [System.ComponentModel.ToolboxItem(false)]
+    [System.ComponentModel.ToolboxItem(true)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     [System.Web.Script.Services.ScriptService]
     public class CalculatorWebService : System.Web.Services.WebService
     {
 
-        [WebMethod]
-        public string HelloWorld()
+        [WebMethod(Description = "This method adds two numbers")]
+        public double Add(Inputs inputs)
         {
-            return "Hello World";
+            return (inputs.firstnumber + inputs.secondnumber);
+        }
+        [WebMethod(Description = "This method divides two numbers")]
+        public double Divide(Inputs inputs)
+        {
+            return (inputs.firstnumber / inputs.secondnumber);
+        }
+        [WebMethod(Description = "This method determines the product of two numbers")]
+        public double Multiply(Inputs inputs)
+        {
+            return (inputs.firstnumber * inputs.secondnumber);
+        }
+        [WebMethod(Description = "This method determines the difference between two numbers")]
+        public double Subtract(Inputs inputs)
+        {
+            return (inputs.firstnumber - inputs.secondnumber);
+        }
+
+
+        [WebMethod(Description = "Returns the Calculations saved in the database")]
+        public List<Calculations> getList()
+        {
+            //var calcList = get all list;
+            var ls = new List<Calculations>();
+            return ls;
+        }
+        [WebMethod(Description = "This method writes the recent calculation into the DB")]
+        public bool insertData(Compute com)
+        {
+            return true;
         }
     }
 }
---
+++ CalculatorWebService/Helpers/Calculations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculatorWebService.Helpers
+{
+    public class Calculations
+    {
+        public int Id { get; set; }
+        public string Operation { get; set; }
+    }
+}
