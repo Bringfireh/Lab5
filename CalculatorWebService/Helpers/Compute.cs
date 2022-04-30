e546354a6d7ebde2493d52ded27fd11bed937b1d
Bringfireh <ngutortsenongo@gmail.com>
2022-04-30 15:49:16 +0100

    added helper classes

---
+++ CalculatorWebService/Helpers/Compute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculatorWebService.Helpers
+{
+    public class Compute
+    {
+        public double InputA { get; set; }
+        public double InputB { get; set; }
+        public double Result { get; set; }
+
+    }
+}
---
+++ CalculatorWebService/Helpers/Inputs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculatorWebService.Helpers
+{
+    public class Inputs
+    {
+        public double firstnumber { get; set; }
+        public double secondnumber { get; set; }
+    }
+}
